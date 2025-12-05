4d7aebfc46db28d9defd84a72baa3730f81329f0
jnm2 <me@jnm2.com>
2025-12-04 19:19:40 -0500

    Maintain separate approaches for netfx and netcore

---
+++ src/netfx-parent/MainApp.Tests/ChildProcessClientTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace MainApp.Tests;
+
+public class ChildProcessClientTests
+{
+    [Test]
+    public void Verify_child_process_behavior()
+    {
+        // Demonstrates that the child process subfolder is transitively copied to the output of the test project.
+        using var client = new ChildProcessClient();
+
+        client.StandardOutput.ReadLine().ShouldStartWith("Hello from .NET Framework 4.8");
+
+        client.StandardInput.WriteLine("String from Verify_child_process_behavior");
+        client.StandardOutput.ReadLine().ShouldBe("""Received "String from Verify_child_process_behavior" from the parent process""");
+    }
+}
